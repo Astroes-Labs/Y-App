bde85cd338eeea31714c75e978bbfbe311374918
Astroes-Labs <astroeslabs@gmail.com>
2024-03-22 02:22:53 +0100

    initial Commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Idea;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        $ideas = Idea::orderBy('created_at', 'DESC');
+
+       if(request()->has('search')){
+            $ideas = $ideas->where('content','like','%'.request()->get('search','').'%');
+       }
+        //dump(Idea::all());
+        
+
+        return view('dashboard',[
+            'ideas' => $ideas->paginate(5),
+            'notviewing' => true,
+        ]);
+    }
+}
